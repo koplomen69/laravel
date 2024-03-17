d2a722f3d6c91d6a63d901e9e728f845f7954ef0
shaquille <69906378+koplomen69@users.noreply.github.com>
2024-03-17 14:36:01 +0700

    Update web.php

--- shaquille/routes/web.php
+++ shaquille/routes/web.php
@@ -5,61 +5,59 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/basic_routing', function () {
-    return 'Hello World';
-});
-
+// Route::get('/basic_routing', function () {
+//     return 'Hello World';
+// });
+Route::view('/clone', 'clone');
 Route::view('/view_route', 'view_route');
-Route::view('/view_route', 'view_route', ['name' => 'Purnama']);
+Route::view('/view_route', 'view_route', ['name' => 'shaquille']);
 Route::get('/controller_route', [RouteController::class, 'index']);
-Route::redirect('/', '/routing');
+// Route::redirect('/', '/routing');
 
-Route::get('/user/{id}', function ($id) {
-    return "User Id: " . $id;
-});
-Route::get('/posts/{post}/comments/{comment}', function (
-    $postId,
-    $commentId
-) {
-    return "Post Id: " . $postId . ", Comment Id: " . $commentId;
-});
+// Route::get('/user/{id}', function ($id) {
+//     return "User Id: " . $id;
+// });
+// Route::get('/posts/{post}/comments/{comment}', function (
+//     $postId,
+//     $commentId
+// ) {
+//     return "Post Id: " . $postId . ", Comment Id: " . $commentId;
+// });
 
-Route::get('username/{name?}', function ($name = null) {
-    return 'Username: ' . $name;
-});
+// Route::get('username/{name?}', function ($name = null) {
+//     return 'Username: ' . $name;
+// });
 
-Route::get('/title/{title}', function ($title) {
-    return "Title: " . $title;
-})->where('title', '[A-Za-z]+');
+// Route::get('/title/{title}', function ($title) {
+//     return "Title: " . $title;
+// })->where('title', '[A-Za-z]+');
 
-Route::get('/profile/{profileId}', [
-    RouteController::class,
-    'profile'
-])->name('profileRouteName');
+// Route::get('/profile/{profileId}', [
+//     RouteController::class,
+//     'profile'
+// ])->name('profileRouteName');
 
 
-//Route::get('/route_priority/{rpId}', function ($rpId) {
-//    return "This is Route One";
-//});
-Route::get('/route_priority/user', function () {
-    return "This is Route 1";
-});
-Route::get('/route_priority/user', function () {
-    return "This is Route 2";
-});
+// //Route::get('/route_priority/{rpId}', function ($rpId) {
+// //    return "This is Route One";
+// //});
 
-Route::fallback(function () {
-    return 'This is Fallback Route';
-});
+// Route::get('/route_priority/user', function () {
+//     return "This is Route 2";
+// });
 
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/dashboard', function () {
-        return "This is admin dashboard";
-    })->name('dashboard');
-    Route::get('/users', function () {
-        return "This is user data on admin page";
-    })->name('users');
-    Route::get('/items', function () {
-        return "This is item data on admin page";
-    })->name('items');
-});
+// Route::fallback(function () {
+//     return 'This is Fallback Route';
+// });
+
+// Route::prefix('admin')->name('admin.')->group(function () {
+//     Route::get('/dashboard', function () {
+//         return "This is admin dashboard";
+//     })->name('dashboard');
+//     Route::get('/users', function () {
+//         return "This is user data on admin page";
+//     })->name('users');
+//     Route::get('/items', function () {
+//         return "This is item data on admin page";
+//     })->name('items');
+// });
