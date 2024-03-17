e6b009ddaaa43cc3c927b3e463368f6dafd48385
shaquille <69906378+koplomen69@users.noreply.github.com>
2024-03-17 17:14:06 +0700

    Update Controller.php

--- shaquille/app/Http/Controllers/Controller.php
+++ shaquille/app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index() {
+        return "This is from Controller";
+        }
+    public function profile($profileId) {
+        return "This is Profile from Controller, profile id: ".$profileId;
+    }
 }
